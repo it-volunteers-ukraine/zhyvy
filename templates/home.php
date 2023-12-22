611e10b11579514e39a61d91a339fd9233710306
Olga Cygankowa <olga.cygankowa@gmail.com>
2023-12-22 13:05:42 +0100

    Add css variables and page container

--- templates/home.php
+++ templates/home.php
@@ -1,11 +1,17 @@
 <?php
-/*
-Template Name: home
-*/
-get_header();
+	/*
+	Template Name: home
+	*/
+	get_header();
 ?>
-<h2>home page</h2>
 
+    <main>
+
+        <section class="container">
+            <h2>home page</h2>
+        </section>
+
+    </main>
 
 
 <?php get_footer(); ?>
