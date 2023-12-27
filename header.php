ec24dcb9508134eab364580807ce228975ec2ea8
Olga Cygankowa <olga.cygankowa@gmail.com>
2023-12-27 19:02:16 +0100

    Add markup and styles for mobile menu

--- header.php
+++ header.php
@@ -7,13 +7,14 @@
 	<?php wp_head(); ?>
     <title>It-volunteers</title>
 </head>
+
 <body>
 <div class="wrapper">
     <header class="header">
         <div class="container">
             <div class="header-content">
 
-                <div class="header-logo">
+                <div class="logo">
 					<?php
 						if ( has_custom_logo() ) {
 							echo get_custom_logo();
@@ -21,24 +22,28 @@
 					?>
                 </div>
 
-                <div class="header-menu">
-                    <div class="burger-menu__overlay"></div>
-
+                <div class="menu menu--mobile">
                     <nav class="menu-nav">
-							<?php wp_nav_menu( [
-								'theme_location' => 'header',
-								'container'      => false,
-								'menu_id'        => false,
-								'echo'           => true,
-								'items_wrap'     => '<ul id="%1$s" class="menu-list %2$s">%3$s</ul>',
-							] );
-							?>
+						<?php wp_nav_menu( [
+							'theme_location' => 'header',
+							'container'      => false,
+							'menu_id'        => false,
+							'echo'           => true,
+							'items_wrap'     => '<ul id="%1$s" class="menu-list %2$s">%3$s</ul>',
+						] );
+						?>
+                        <a class="button--header button--header--mobile"
+                           href="<?php echo get_permalink( get_page_by_path( 'pidtrymaty' ) ); ?>">підтримати
+                        </a>
                     </nav>
 
-                    <a class="button--header"
-                       href="<?php echo get_permalink( get_page_by_path('pidtrymaty') ); ?>">підтримати
+                    <button class="menu-toggle"></button>
+
+                    <a class="button--header button--header--desktop"
+                       href="<?php echo get_permalink( get_page_by_path( 'pidtrymaty' ) ); ?>">підтримати
                     </a>
                 </div>
+
             </div>
         </div>
     </header>
