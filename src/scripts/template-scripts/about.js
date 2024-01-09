2bd7b8958e6f59b9f3e5bf93e363b83653ef68f1
Ev Bludov <120216330+ernestoDug@users.noreply.github.com>
2024-01-09 22:37:42 +0200

    Mxnt13 a bout (#18)
    * 1
    
    * 2
    
    * 4
    
    * 6
    
    * 7

--- src/scripts/template-scripts/about.js
+++ src/scripts/template-scripts/about.js
@@ -1,1 +1,60 @@
-console.log('about');
+// Results_section
+
+const swiperResults = new Swiper(".swiperResults", {
+  centeredSlides: true,
+  centeredSlidesBounds: true,
+  createElements: true,
+  effect: "cube", // experiment style cube
+  spaceBetween: 0,
+  slidesPerView: 2,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// honors-and-thanks__section
+
+const swiperHonor = new Swiper(".swiperHonor", {
+  breakpoints: {
+    1920: {
+      spaceBetween: 20,
+      slidesPerView: 5,
+    },
+
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 16,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 16,
+    },
+    360: {
+      slidesPerView: 1,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// Lightbox
+
+function initializeLightbox() {
+  lightbox.option({
+    resizeDuration: 200,
+    alwaysShowNavOnTouchDevices: true,
+    disableScrolling: true,
+    albumLabel: "Слайд %1 з %2",
+    fitImagesInViewport: true,
+    positionFromTop: 200,
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  initializeLightbox();
+});
+
+console.log("skdmosdkoslmd");
