08b4db73fb2c6c910dc5d706b2b90e84f7e25c7e
Olga Cygankowa <olga.cygankowa@gmail.com>
2024-01-13 14:54:27 +0100

    Add mini card for events

---
+++ template-parts/content-event-mini-card.php
@@ -0,0 +1,38 @@
+<article class="content-event-mini-card">
+    <a class="card-wrapper" href="<?php the_permalink(); ?>">
+
+        <div class="image">
+            <img src="<?php echo esc_url( get_field( 'img' )['url'] ); ?>"
+                 alt="<?php echo esc_attr( get_field( 'img' )['alt'] ); ?>">
+        </div>
+
+        <div class="description">
+            <div class="tags">
+				<?php $postCategories = get_the_category( get_the_ID() );
+					if ( ! empty( $postCategories ) ) :
+						foreach ( $postCategories as $category ) :
+							$categoryColor = get_field( 'color', $category )['0'] ?? '';
+							?>
+                            <span class="tag--small tag--<?php echo $categoryColor ?>"><?php echo esc_html( $category->name ) ?></span>
+						<?php endforeach; ?>
+					<?php endif; ?>
+            </div>
+
+            <p class="datetime">
+                <span class="date">
+                    <svg width="16" height="16">
+                    <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/sprite.svg#icon-calendar"></use>
+                    </svg>
+                    <?php the_field( 'date' ); ?>
+                </span>
+                <span class="time">
+                    <svg width="16" height="16">
+                    <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/sprite.svg#icon-clock"></use>
+                    </svg>
+                    <?php the_field( 'time' ); ?>
+                </span>
+            </p>
+            <h4><?php the_field( 'title' ); ?></h4>
+        </div>
+    </a>
+</article>
