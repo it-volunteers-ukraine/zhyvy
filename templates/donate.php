bea0a44ad64614e19e2d01c591534d2e6f2de09a
MViktoriia <88782163+MViktoriia@users.noreply.github.com>
2024-01-13 09:32:42 +0200

    murkup

--- templates/donate.php
+++ templates/donate.php
@@ -4,89 +4,45 @@
 	*/
 	get_header();
 ?>
-    <h2>donate page</h2>
 
     <main>
-        <section class="container">
-            <div style="background: #2d3136; padding: 20px; margin-top: 40px">
-                <h2 class="title-gradient-h1">title gradient h1</h2>
+        <?php get_template_part( 'template-parts/page-banner', null, array(
+                'title' => get_field( "title" )
+            ) ); ?>
+        <section class="donate">
+            <div class="container">
+                <h1 class="title-gradient-h1"><?php echo esc_html( get_field('title') ); ?></h1>
+                <div class="donate-text-wrapper">
+                    <?php echo wp_kses_post ( get_field('text') ); ?>
+                </div>
+                <div class="donate-payment-details">
+                    <div class="qr-code-wrapper">
+                        <?php if( get_field('qrcode') ): ?>
+                            <img src="<?php the_field('qrcode'); ?>" alt="QR-code для донату" />
+                        <?php endif; ?>
+                    </div>
+                    <div class="payment-details-wrapper">
+                        <h2 class="title-gradient-h2"><?php echo esc_html( get_field('header_donate') ); ?></h2>
+                        <p>Розрахунковий рахунок ГО ЖИВИ:</p>
+                        <p><?php echo esc_html( get_field('account') ); ?></p>
+                        <div class="">
+                            <svg width="16" height="16">
+                                <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/sprite.svg#icon-copy"></use>
+                            </svg>
+                        </div>
+                        <p>Картка Приват Банку:</p>
+                        <p><?php echo esc_html( get_field('card_holder') ); ?></p>
+                        <p><?php echo esc_html( get_field('card_number') ); ?></p>
+                        <div class="">
+                            <svg width="16" height="16">
+                                <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/sprite.svg#icon-copy"></use>
+                            </svg>
+                        </div>
+                    </div>
+                </div>
 
-                <h2 class="title-gradient-h2">title gradient h2</h2>
 
-                <h2 class="title-page">title page</h2>
-
-                <h2 class="title-event">title event</h2>
-            </div>
-
-            <div style="background: #2d3136; padding: 20px; margin-top: 40px">
-
-                <a href="#" class="button--header">button header</a>
-
-                <a href="#" class="button--email">button email</a>
-
-                <a href="#" class="button--small">button small</a>
-
-                <a href="#" class="button--medium">button medium</a>
-
-                <a href="#" class="button--large">button large
-                    <svg width="8" height="16">
-                        <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/symbol-defs.svg#chevron-right"></use>
-                    </svg>
-                </a>
-
-                <br>
-                <br>
-
-                <a href="#" class="button--outlined">
-                <span>
-                    button outlined
-                  <i class="icon-chevron-right"></i>
-                </span>
-                </a>
-
-                <br>
-                <br>
-
-                <a href="#" class="button-arrow--small">
-                    <svg width="8" height="16">
-                        <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/symbol-defs.svg#chevron-right"></use>
-                    </svg>
-                </a>
-
-                <a href="#" class="button-arrow--medium">
-                    <svg width="12" height="24">
-                        <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/symbol-defs.svg#chevron-right"></use>
-                    </svg>
-                </a>
-
-                <a href="#" class="button-arrow--up">
-                    <svg width="16" height="32" style="transform: rotate(-90deg);">
-                        <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/symbol-defs.svg#chevron-right"></use>
-                    </svg>
-                </a>
-
-                <br>
-                <br>
-
-                <span class="tag--small tag--green">
-                    Small tag
-                </span>
-
-                <span class="tag--large tag--green">
-                    Large tag
-                </span>
-
-                <span class="tag--large tag--green">
-                    Online
-                </span>
-
-                <span class="tag--large tag--orange">
-                    Offline
-                </span>
-
-                <span class="tag--large tag--violet">
-                    Щонеділі
-                </span>
+               
             </div>
         </section>
     </main>
