e22976746b15aff973459412a3baa0e68e979c8c
Pavel-K-ua <113534910+Pavel-K-ua@users.noreply.github.com>
2024-01-16 14:26:35 +0200

    add new footer

--- src/scripts/footer.js
+++ src/scripts/footer.js
@@ -1,1 +1,9 @@
-console.log('footer part');
+console.log("footer part");
+
+const dropDownBtn = document.querySelector("#footerDropDownBtn");
+const dropDownMenu = document.querySelector("#footerDropDownMenu");
+
+dropDownBtn.addEventListener("click", () => {
+  if (window.innerWidth <= 992)
+    dropDownMenu.classList.toggle("footer-menu-item-wrapper");
+});
