0aae6ef0a0b82c969280b6b3bcb02e7a85ccdce8
Olga Cygankowa <olga.cygankowa@gmail.com>
2024-02-02 10:25:08 +0100

    Change code formatting

--- src/scripts/template-scripts/about.js
+++ src/scripts/template-scripts/about.js
@@ -1,108 +1,107 @@
 // team_section ****************************
 
-const listTeam = document.querySelector(".team_list");
+const listTeam = document.querySelector('.team_list');
 
-listTeam.addEventListener("click", openTmCard);
+listTeam.addEventListener('click', openTmCard);
 
 function openTmCard(event) {
-  let target = event.target;
+    let target = event.target;
 
-  if (target.className === "front-sideIMG") {
-    target.closest("div").classList.add("rotate-180");
+    if (target.className === 'front-sideIMG') {
+        target.closest('div').classList.add('rotate-180');
 
-    target
-      .closest("div")
-      .nextSibling.nextElementSibling.classList.add("rotate-0");
-  }
+        target
+            .closest('div')
+            .nextSibling.nextElementSibling.classList.add('rotate-0');
+    }
 
-  if (target.className === "back-side_text") {
-    target.closest("div").classList.remove("rotate-0");
-    target
-      .closest("div")
-      .previousSibling.previousElementSibling.classList.remove("rotate-180");
-  }
+    if (target.className === 'back-side_text') {
+        target.closest('div').classList.remove('rotate-0');
+        target
+            .closest('div')
+            .previousSibling.previousElementSibling.classList.remove('rotate-180');
+    }
 }
 
 // Results_section*************************************
 
-const swiperResults = new Swiper(".swiperResults", {
-  centeredSlides: true,
-  loop: true,
+const swiperResults = new Swiper('.swiperResults', {
+    centeredSlides: true,
+    loop: true,
 
-  breakpoints: {
-    1920: {
-      spaceBetween: 5,
-      slidesPerView: 3,
+    breakpoints: {
+        1920: {
+            spaceBetween: 5,
+            slidesPerView: 3,
+        },
+
+        992: {
+            spaceBetween: 5,
+            slidesPerView: 3,
+        },
+        768: {
+            spaceBetween: 5,
+            slidesPerView: 3,
+        },
+
+        360: {
+            spaceBetween: 10,
+            slidesPerView: 1,
+        },
     },
 
-    992: {
-      spaceBetween: 5,
-      slidesPerView: 3,
+    navigation: {
+        nextEl: '.button-next',
+        prevEl: '.button-prev',
     },
-    768: {
-      spaceBetween: 5,
-      slidesPerView: 3,
-    },
-
-    360: {
-      spaceBetween: 10,
-      slidesPerView: 1,
-    },
-  },
-
-  navigation: {
-    nextEl: ".button-next",
-    prevEl: ".button-prev",
-  },
 });
 
 // honors-and-thanks__section*************************************
 
-const swiperHonor = new Swiper(".swiperHonor", {
-  centeredSlides: true,
-  loop: true,
-  spaceBetween: 10,
+const swiperHonor = new Swiper('.swiperHonor', {
+    centeredSlides: true,
+    loop: true,
+    spaceBetween: 10,
 
-  breakpoints: {
-    1920: {
-      spaceBetween: 20,
-      slidesPerView: 5,
-    },
+    breakpoints: {
+        1920: {
+            spaceBetween: 20,
+            slidesPerView: 5,
+        },
 
-    992: {
-      slidesPerView: 3,
-      spaceBetween: 16,
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 16,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 16,
+        },
+        360: {
+            slidesPerView: 1,
+            spaceBetween: 16,
+        },
     },
-    768: {
-      slidesPerView: 3,
-      spaceBetween: 16,
+    navigation: {
+        nextEl: '.button-next',
+        prevEl: '.button-prev',
     },
-    360: {
-      slidesPerView: 1,
-      spaceBetween: 16,
-    },
-  },
-  navigation: {
-    nextEl: ".button-next",
-    prevEl: ".button-prev",
-  },
 });
 
 // Lightbox***********************************************
 
 function initializeLightbox() {
-  lightbox.option({
-    resizeDuration: 200,
-    alwaysShowNavOnTouchDevices: true,
-    disableScrolling: true,
-    albumLabel: "Слайд %1 з %2",
-    fitImagesInViewport: true,
-    positionFromTop: 200,
-  });
+    lightbox.option({
+        resizeDuration: 200,
+        alwaysShowNavOnTouchDevices: true,
+        disableScrolling: true,
+        albumLabel: 'Слайд %1 з %2',
+        fitImagesInViewport: true,
+        positionFromTop: 200,
+    });
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  initializeLightbox();
+document.addEventListener('DOMContentLoaded', function () {
+    initializeLightbox();
 });
 
-console.log("skdmosdkoslmd");
